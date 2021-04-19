9f13df1c6b63b1d66bf32ea496e7574c4c6796c2
dxc7649 <42787527+dxc7649@users.noreply.github.com>
2021-04-19 14:03:31 -0400

    committed client folder

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 3001;
 const unirest = require("unirest");
 const API_KEY = "731d3b8d-1843-4f12-8957-5ac68ffb586d";
 
+// import path library
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/associations/:word', (req, res) => {
     const word = req.params.word;
     const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
@@ -19,6 +25,12 @@ app.get('/api/associations/:word', (req, res) => {
                 message: `${error}`
             });
         });
+});
+
+// put this AFTER the other `app.get(...`
+// for any request that doesn't match one above, send back React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port, () => {
